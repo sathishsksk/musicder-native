6aa1d9a56cdf40daa03649ac1a50d8d5c8cdf757
sathishsksk <67107485+sathishsksk@users.noreply.github.com>
2022-12-19 08:03:57 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -70,7 +70,7 @@ function searchF() {
         }
     });
 
-    search.open("GET", "https://songapi.thetuhin.com/search?query=" + songquery);
+    search.open("GET", "https://jiosaavn-2022.vercel.app/search?query=" + songquery);
     search.send();
 }
 
